0116670c8d04d6d84b751c7099b25ed848d7df60
arungoud23 <100275592+arungoud23@users.noreply.github.com>
2022-05-23 19:29:30 +0530

    done

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
   // NOTE: do not delete `data-cy` key value pair
+  let [items,setItem] = useState("")
+  let [data,setData] = useState([])
+
+  const addItem = () =>{
+    if(! items){
+
+    }else{
+      setData([...data,items])
+      setItem("")
+    }
+  }
+
+  const deleteItem = (id) => {
+    const updatedItems = data.filter((elem,ind)=>{
+      return ind !== id;
+    })
+    setData(updatedItems);
+  }
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
-    </div>
+      <input data-cy="add-task-input" type="text" placeholder="...Add items" value={items} onChange={(e) => setItem(e.target.value)} />
+      <button data-cy="add-task-button" onClick={addItem}>Add items</button>
+
+      {/* <div className="showItem"> */}
+        {
+          data.map((elem,ind)=>{
+            return(
+              <div className="eachItem" key={ind}>
+                <h3>{elem}
+                <i className="far fa-trash-alt addbtn" title="DeleteItem" onClick={() => deleteItem(ind)}></i></h3>
+                </div>
+            )
+          })
+        }
+      </div>
+    // </div>
+    
   );
 };
 
